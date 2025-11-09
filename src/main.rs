88376d78d1356a89ea25a20f5cdf923f4888ef46
Redkin-Aleksey <redkin0aleksey@gmail.com>
2025-11-09 23:01:59 +0300

    feat: add homework code to src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{self, Read};
+use std::fs;
+
 fn main() {
 
+    let mut file_path = String::new();
+
+    if let Err(e) = io::stdin().read_line(&mut file_path) {
+        eprintln!("input error: {}", e);
+        return;
+    }
+
+    let file_path = file_path.trim();
+
+    match fs::File::open(file_path) {
+        Ok(mut file) => {
+            let mut test = String::new();
+            match file.read_to_string(&mut test) {
+                Ok(_) => println!("success"),
+                Err(_) => println!("failure")
+            }
+        }
+        Err(_) => println!("failure")
+    }
+
 }
+
